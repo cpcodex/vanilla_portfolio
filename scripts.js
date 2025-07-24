06dcc6b67a192361145c67a0ad1d7d1715ecbdfb
Nick🛰️ <cpcodex23@gmail.com>
2025-07-23 22:47:01 -0400

    update hamburger menu, init js script for menu handling

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ const header = document.querySelector('.main-header');
 hamburgerButton.addEventListener('click', () => {
     header.classList.toggle('is-menu-open');
   });
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    header.classList.remove('is-menu-open');
+  }
+});
