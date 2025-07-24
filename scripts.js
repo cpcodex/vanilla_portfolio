bc1cbc628131cadbfb02c5ef86c314205611f105
Nick🛰️ <cpcodex23@gmail.com>
2025-07-24 08:10:16 -0400

    init contact_me; logic and styling implemented, emailjs tested and running.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,42 @@
-// hamburger button
-const hamburgerButton = document.querySelector('.hamburger-menu');
-const header = document.querySelector('.main-header');
-
-hamburgerButton.addEventListener('click', () => {
-    header.classList.toggle('is-menu-open');
+window.onload = function () {
+    
+  // hamburger menu logic
+  const hamburgerButton = document.querySelector('.hamburger-menu');
+  const header = document.querySelector('.main-header');
+  if (hamburgerButton) { // Check if the button exists
+      hamburgerButton.addEventListener('click', () => {
+          header.classList.toggle('is-menu-open');
+      });
+  }
+    
+  window.addEventListener('resize', () => {
+      if (window.innerWidth > 768) {
+          if (header) {
+              header.classList.remove('is-menu-open');
+          }
+      }
   });
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 768) {
-    header.classList.remove('is-menu-open');
+  // EmailJS logic
+  const contactForm = document.getElementById('contact-form');
+    
+  if (contactForm) {
+      emailjs.init({
+          publicKey: "eSvkFlyJcU-cBB_Rd",
+      });
+
+      contactForm.addEventListener('submit', function (event) {
+          event.preventDefault();
+            
+          emailjs.sendForm('service_odfd4eo', 'template_main_con', this)
+              .then(() => {
+                  console.log('SUCCESS!');
+                  alert('Your message has been sent successfully!');
+                  contactForm.reset();
+              }, (error) => {
+                  console.log('FAILED...', error);
+                  alert('Failed to send the message. Please try again.');
+              });
+      });
   }
-});
+};
